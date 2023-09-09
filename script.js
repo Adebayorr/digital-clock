673a722db09832ff9dd4de8b2b4b700cf2bec97d
user <raheemadebayorr@gmail.con>
2023-09-09 09:01:35 +0100

    remove the redundant console log

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const getTimeAndDate = () => {
     const time = new Date()
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    console.log(time.getDay())
     document.querySelector('.day').textContent = days[time.getDay()]
     document.querySelector('.time').textContent = Date.now()
 
