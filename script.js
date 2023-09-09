69ca7f203e78730f9dbf5a0a8dfdf41d3b59b2d4
user <raheemadebayorr@gmail.con>
2023-09-09 08:59:08 +0100

    fixed some bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getTimeAndDate = () => {
     const time = new Date()
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']
-    console.log(time.getMilliseconds())
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    console.log(time.getDay())
     document.querySelector('.day').textContent = days[time.getDay()]
     document.querySelector('.time').textContent = Date.now()
 
